1e1ce325145c9270741fdd589d0ec7039e798bb5
Tsukii007 <tyagibhavyansh123@gmail.com>
2025-08-27 11:42:59 +0530

    4.c

---
+++ 4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+  float radius, area, circum;
+  float pi = 3.1416;
+  printf("Enter the radius of the circle: ");
+  scanf("%f", &radius);
+  area = pi * radius * radius;
+  circum = 2 * pi * radius;
+  printf("Area = %.2f,Circumference = %.2f\n", area, circum);
+  return 0;
+}
