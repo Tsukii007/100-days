9f015eda0f2017d8af8eb2ad2edcf190fc369ad9
Tsukii007 <tyagibhavyansh123@gmail.com>
2025-08-27 11:39:14 +0530

    3.c

---
+++ 3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    float len, bre;
+    printf("Enter length and breadth of the rectangle: ");
+    scanf("%f %f", &len, &bre):
+    float area,perimeter;
+    area = len * bre;
+    perimeter = 2 * (length + breadth);
+    printf("Area = %.2f,Perimeter = %.2f\n", area, perimeter);
+    return 0;
+}
