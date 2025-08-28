50754405874e7233145cce4d6645bbbc5ffa6038
Bhavyansh <tyagibhavyansh123@gmail.com>
2025-08-28 21:28:09 +0530

    Create 14.c

---
+++ 14.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+    char ch;
+    printf("Enter a character: ");
+    scanf("%c", &ch);
+    if (ch >= 'A' && ch <= 'Z') {
+        ch = ch + 32;
+    }
+    if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+        printf("Vowel\n");
+    } 
+    else if (ch >= 'a' && ch <= 'z') {
+        printf("Consonant\n");
+    } 
+    else {
+        printf("Invalid input! Please enter an alphabet.\n");
+    }
+    return 0;
+}
