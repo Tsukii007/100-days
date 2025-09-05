b6a5b9465e673b63c2617793f341fb47f03e5e3e
Bhavyansh <tyagibhavyansh123@gmail.com>
2025-09-05 23:31:08 +0530

    Create 28.c

---
+++ 28.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int n, i;
+    int product = 1;
+    printf("Enter n: ");
+    scanf("%d", &n);
+    for (i = 2; i <= n; i += 2) {
+        product *= i;
+    }
+    printf("%d\n", product);
+    return 0;
+}
