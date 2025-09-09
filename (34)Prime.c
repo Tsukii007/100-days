bb5ad1aa3e2a6f3bab6608694f309cd6391de4c1
Bhavyansh <tyagibhavyansh123@gmail.com>
2025-09-09 23:32:33 +0530

    Update and rename 34.c to (34)Prime.c

--- (34)Prime.c
+++ (34)Prime.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
-#include <math.h>
 int main() {
     int n, i, isPrime = 1;
-    printf("Enter a number: ");
     scanf("%d", &n);
     if (n <= 1) {
         isPrime = 0;
-    } 
+    }
     else {
-        for (i = 2; i <= sqrt(n); i++) {
+        for (i = 2; i * i <= n; i++) {
             if (n % i == 0) {
                 isPrime = 0;
                 break;
@@ -16,9 +14,8 @@ int main() {
         }
     }
     if (isPrime)
-        printf("Prime\n");
+        printf("Prime");
     else
-        printf("Not prime\n");
-
+        printf("Not prime");
     return 0;
 }
