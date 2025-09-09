9f07d0e285a735e1294d9c5419e66cbd1ece7f29
Bhavyansh <tyagibhavyansh123@gmail.com>
2025-09-09 14:29:22 +0530

    Update and rename 1.c to inputandsum.c

---
+++ inputandsum.c
@@ -0,0 +1,9 @@
+/* Input two numbers and output their sum */
+#include <stdio.h>
+int main() {
+    int num1, num2, sum;
+    scanf("%d %d", &num1, &num2); //taking input
+    sum = num1 + num2;  //adding them
+    printf("Sum = %d\n",sum); //printing their sum
+    return 0;
+}
