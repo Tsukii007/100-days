8527c2256c696f8af0a1d0ad2837db1f112a2933
Bhavyansh <tyagibhavyansh123@gmail.com>
2025-09-10 00:20:08 +0530

    Create (42)Perfect.c

---
+++ (42)Perfect.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+    int num, i, sum = 0;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    for (i = 1; i <= num / 2; i++) {
+        if (num % i == 0) {
+            sum += i;
+        }
+    }
+    if (sum == num && num != 0)
+        printf("Perfect number\n");
+    else
+        printf("Not perfect number\n");
+
+    return 0;
+}
