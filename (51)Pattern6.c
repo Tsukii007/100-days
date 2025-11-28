f56d3c8672477a1697c544178fdf141315286c12
BHAVYANSH TYAGI <tyagibhavyansh123@gmail.com>
2025-11-28 17:55:18 +0530

    Update and rename 51.c to (51)Pattern6.c

--- (51)Pattern6.c
+++ (51)Pattern6.c
@@ -1,21 +1,15 @@
 #include <stdio.h>
-
 int main() {
     int i, j, space;
-    int n = 5; // Number of rows
-
-    // Loop for rows
+    int n = 5;
     for (i = 5; i >= 1; i--) {
-        // Print spaces before numbers
         for (space = 1; space < i; space++) {
             printf(" ");
         }
-        // Print numbers from i to 5
         for (j = i; j <= 5; j++) {
             printf("%d", j);
         }
-        printf("\n"); // Move to next line
+        printf("\n");
     }
-
     return 0;
 }
