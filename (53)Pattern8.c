62638b42d2f292ce0a6cb307591d65e27408d206
BHAVYANSH TYAGI <tyagibhavyansh123@gmail.com>
2025-11-28 17:56:31 +0530

    Update and rename 53.c to (53)Pattern8.c

--- (53)Pattern8.c
+++ (53)Pattern8.c
@@ -1,24 +1,18 @@
 #include <stdio.h>
-
 int main() {
     int i, j;
-    int n = 5; // number of rows in the top half
-
-    // Top half (1, 3, 5, 7, 9)
+    int n = 5;
     for (i = 1; i <= n; i++) {
         for (j = 1; j <= 2*i - 1; j++) {
             printf("*");
         }
         printf("\n");
     }
-
-    // Bottom half (7, 5, 3, 1)
     for (i = n-1; i >= 1; i--) {
         for (j = 1; j <= 2*i - 1; j++) {
             printf("*");
         }
         printf("\n");
     }
-
     return 0;
 }
