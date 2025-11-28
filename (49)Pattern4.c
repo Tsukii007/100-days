8f685a5e37feaed6df1043992d614d4ebf310daa
BHAVYANSH TYAGI <tyagibhavyansh123@gmail.com>
2025-11-28 17:51:00 +0530

    Update and rename 49.c to (49)Pattern4.c

--- (49)Pattern4.c
+++ (49)Pattern4.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
-
 int main() {
     int i, j;
-
-    // Loop for rows
     for (i = 5; i >= 1; i--) {
-        // Print numbers from i to 5
         for (j = i; j <= 5; j++) {
             printf("%d", j);
         }
-        printf("\n"); // Move to next line
+        printf("\n");
     }
-
     return 0;
 }
